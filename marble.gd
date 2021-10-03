760f48b90b4b94ecc34474357eaabab9323798a0
hannah-ronan <hannah.ronan6@gmail.com>
2021-10-03 17:44:37 -0400

    fixed click not working
    fixed mouse click not working by setting table gridcontainer mouse filter to "ignore"

--- marble.gd
+++ marble.gd
@@ -1,15 +1,18 @@
 extends Node2D
+class_name Marble
+var active = true
 var marble = preload("res://marble.png")
 var marble_clicked = preload("res://marble_clicked.png")
-class Marble:
-	var active = true
-	func move(direction):
-		pass
+func move(direction):
+	pass
 	
-func _on_input_event(viewport, event, shape_idx):
+func on_click():
+	print("click")
+	$Area2D/Sprite.set_texture(marble_clicked)
+	
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.is_pressed():
 		#when the mouse clicks the marble
-		on_click()
+		self.on_click()
 
-func on_click():
-	$Area2D/Sprite.set_texture(marble_clicked)
